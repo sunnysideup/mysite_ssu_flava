d2f3b3b7a8d9bd367fc25317d674923ed175721f
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2013-11-13 02:00:59 +0000

    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/mysite/trunk@5385 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- _config.php
+++ _config.php
@@ -16,33 +16,9 @@ date_default_timezone_set("NZ");
 
 
 //===================---------------- START sapphire MODULE ----------------===================
-SSViewer::set_theme('main');
-MySQLDatabase::set_connection_charset('utf8');
-//SS3 obsolete: Geoip::$default_country_code = "NZ";
-GD::set_default_quality(85);
-Email::setAdminEmail('swd@sunnysideup.co.nz');
-//Member::set_password_validator( new NZGovtPasswordValidator()); //this does not really work
-//SS3 obsolete: SiteTree::$breadcrumbs_delimiter = ' <span class="delimiter">&raquo;</span> ';
-Session::set_timeout(1209600);//60 * 60 * 24 * 14
-Email::bcc_all_emails_to('copyonly@sunnysideup.co.nz');
-//Requirements::set_combined_files_folder("themes/main"); //DO NOT USE!!!
 FulltextSearchable::enable(array("SiteTree"));
 if(Director::isLive()) {
 	Director::forceWWW();
 	SS_Log::add_writer(new SS_LogEmailWriter('ssuerrors@gmail.com'), SS_Log::ERR);
 }
-else {
-	Email::send_all_emails_to("swd@sunnysideup.co.nz");
-	BasicAuth::protect_entire_site();
-	if(Director::isDev()) {
-		SSViewer::set_source_file_comments(true);
-	}
-}
-i18n::set_locale('en_NZ');
-i18n::set_date_format("dd-MM-YYYY");
-
-LeftAndMain::setApplicationName("Sunny Side Up Website");
-LeftAndMain::require_css("mysite/css/cmsfixes.css");
-//SS3 obsolete: LeftAndMain::setLogo($location = "", $style = "");
-//SS3 obsolete: LeftAndMain::set_loading_image("themes/main/images/logo.gif");
 //===================---------------- END sapphire MODULE ----------------===================
