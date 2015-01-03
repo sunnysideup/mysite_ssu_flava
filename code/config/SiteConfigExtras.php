82567fc27fd3515278b86b7aa4c6dd3787977039
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2015-01-03 21:25:11 +0000

    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/mysite/trunk@5988 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- code/config/SiteConfigExtras.php
+++ code/config/SiteConfigExtras.php
@@ -17,8 +17,9 @@ class SiteConfigExtras extends DataExtension {
 	);
 
 	function updateCMSFields(FieldList $fields) {
-		$fields->addFieldToTab("Root.PageElements", new HTMLEditorField($name = "CopyrightNotice", $title = "Copyright notice.", 2));
+		$fields->addFieldToTab("Root.PageElements", $editor = new HTMLEditorField($name = "CopyrightNotice", $title = "Copyright notice."));
 		$fields->addFieldToTab("Root.PageElements", new UploadField($name = "BackgroundImage", $title = "Background Image"));
+		$editor->setRows(10);
 		return $fields;
 	}
 
