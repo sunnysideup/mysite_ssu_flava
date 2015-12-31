f7721d38496a9169b0fc4c96a0dcf1cf0f9cc999
helpfulrobot <chris@silverstripe.com>
2016-01-01 05:26:04 +1300

    Converted to PSR-2

--- code/Page.php
+++ code/Page.php
@@ -1,51 +1,55 @@
 <?php
 
-class Page extends SiteTree {
+class Page extends SiteTree
+{
 
-	private static $has_one = array(
-		"BackgroundImage" => "Image"
-	);
+    private static $has_one = array(
+        "BackgroundImage" => "Image"
+    );
 
-	function getCMSFields() {
-		$fields = parent::getCMSFields();
-		return $fields;
-	}
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        return $fields;
+    }
 
-	function MyBackgroundImage() {
-		if($this->BackgroundImageID) {
-			if($image = $this->BackgroundImage()) {
-				return $image;
-			}
-		}
-		if($this->ParentID) {
-			if($parent = SiteTree::get()->byID($this->ParentID)) {
-				return $parent->MyBackgroundImage();
-			}
-		}
-		if($this->URLSegment != 'home') {
-			if($homePage = SiteTree::get()->filter(array('URLSegment' => 'home'))->first()) {
-				return $homePage->MyBackgroundImage();
-			}
-		}
-		if($siteConfig = SiteConfig::current_site_config()) {
-			return $siteConfig->BackgroundImage();
-		}
-	}
+    public function MyBackgroundImage()
+    {
+        if ($this->BackgroundImageID) {
+            if ($image = $this->BackgroundImage()) {
+                return $image;
+            }
+        }
+        if ($this->ParentID) {
+            if ($parent = SiteTree::get()->byID($this->ParentID)) {
+                return $parent->MyBackgroundImage();
+            }
+        }
+        if ($this->URLSegment != 'home') {
+            if ($homePage = SiteTree::get()->filter(array('URLSegment' => 'home'))->first()) {
+                return $homePage->MyBackgroundImage();
+            }
+        }
+        if ($siteConfig = SiteConfig::current_site_config()) {
+            return $siteConfig->BackgroundImage();
+        }
+    }
 }
 
-class Page_Controller extends ContentController {
+class Page_Controller extends ContentController
+{
 
-	function init(){
-		parent::init();
-	}
+    public function init()
+    {
+        parent::init();
+    }
 
-	function Siblings(){
-		if(!$this->ParentID) {
-			$this->ParentID = 0;
-		}
-		return Page::get()
-			->filter(array("ShowInMenus" => 1, "ParentID" => $this->ParentID));
-	}
-
+    public function Siblings()
+    {
+        if (!$this->ParentID) {
+            $this->ParentID = 0;
+        }
+        return Page::get()
+            ->filter(array("ShowInMenus" => 1, "ParentID" => $this->ParentID));
+    }
 }
-
