1824b1f22b0f5af2f74a06ba5b0bbff59ca26813
Nicolaas <github@sunnysideup.co.nz>
2016-04-25 19:06:20 +1200

    adding cache data

--- _config.php
+++ _config.php
@@ -12,16 +12,12 @@ require_once("conf/ConfigureFromEnv.php");
 date_default_timezone_set('Pacific/Auckland');
 //===================---------------- END php MODULE ----------------===================
 
+//set cache to 72 hours
+SS_Cache::set_cache_lifetime('any', 60*60*73);
 
-
-
-//===================---------------- START sapphire MODULE ----------------===================
-FulltextSearchable::enable(array("SiteTree"));
 if(Director::isLive()) {
 	SS_Log::add_writer(new SS_LogEmailWriter('ssuerrors@gmail.com'), SS_Log::ERR);
 }
 else {
 	//BasicAuth::protect_entire_site(); see config.yml
 }
-
-//===================---------------- END sapphire MODULE ----------------===================
