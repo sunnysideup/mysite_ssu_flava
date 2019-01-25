1445f36f94d52daf3cb0f94907b5929572ddccca
Nicolaas <github@sunnysideup.co.nz>
2019-01-26 09:50:45 +1300

    Update Page.php

--- code/Page.php
+++ code/Page.php
@@ -35,37 +35,3 @@ class Page extends SiteTree
     }
 }
 
-class Page_Controller extends ContentController
-{
-    public function init()
-    {
-        parent::init();
-        Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
-    }
-
-    public function Siblings()
-    {
-        if (!$this->ParentID) {
-            $this->ParentID = 0;
-        }
-        return Page::get()
-            ->filter(array("ShowInMenus" => 1, "ParentID" => $this->ParentID));
-    }
-
-    function IsInHouseTemplate()
-    {
-        $standard = array(
-            'Page',
-            'WebPortfolioPage',
-            'PresentationPage',
-            'TermsAndConditionsPage',
-            'ErrorPage',
-            'HomePage',
-            'TypographyTestPage',
-            'TemplateOverviewPage',
-            'UserDefinedForm'
-        );
-
-        return in_array($this->ClassName, $standard);
-    }
-}
